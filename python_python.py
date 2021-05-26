a059861f666ba02e55e3bd6e1eda260afe4eab9e
Afshana123 <nKafuk2JTFW!ATJ>
2021-05-26 14:41:27 +0100

    Updated files

---
+++ python_python.py
@@ -0,0 +1,7 @@
+# Let's use python to make an API call using package called requests
+# dependencies are pip
+
+#pip is the package manager
+
+import requests
+response_bbc = requests.get("hhtps://www.bbc.co.uk/")
