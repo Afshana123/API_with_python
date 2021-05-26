c732d04fc3ff23b97ab8281bd4d3042a20f3e973
Afshana123 <nKafuk2JTFW!ATJ>
2021-05-26 16:10:25 +0100

    Post Code Task

--- python_python.py
+++ python_python.py
@@ -4,4 +4,40 @@
 #pip is the package manager
 
 import requests
-response_bbc = requests.get("hhtps://www.bbc.co.uk/")
+import json
+
+# post_api_response = requests.get("https://api.postcodes.io/postcodes/se120nb")
+# if post_api_response.status_code == 200:
+#     print("Thank you for your request " + str(post_api_response.status_code))
+# else:
+#     print("Sorry, the postcode is incorrect. Please enter the correct postcode.")
+
+
+user_postcode = input("Please enter your postcode:")
+post_api_response = requests.get("https://api.postcodes.io/postcodes/" + user_postcode)
+print(post_api_response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# response_bbc = requests.get("https://www.bbc.co.uk/")
+#
+# # print(response_bbc)
+# # print(response_bbc.status_code)
+#
+# print(response_bbc.headers)
+# data_headers = response_bbc.headers
+# for date_in in data_headers:
+#     print(date_in)
