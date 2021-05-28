6053cdd1e191e063d2458356704953d306807077
Afshana123 <nKafuk2JTFW!ATJ>
2021-05-28 16:19:53 +0100

    Files Updated

--- postcode_task.py
+++ postcode_task.py
@@ -11,6 +11,10 @@ class Validate_postcode:
         post_api_response = requests.get("https://api.postcodes.io/postcodes/" + user_postcode)
         if post_api_response.status_code == 200:
             print("Your postcode is valid.")
+            print(post_api_response.headers)
+            data_headers = post_api_response.headers
+            for date_in in data_headers:
+                print(date_in)
         else:
             print("Sorry, the postcode is incorrect. Please enter the correct postcode.")
 
